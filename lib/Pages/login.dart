f5643f39431a307a9325f7ec338ad206e2638f53
utku <abtulrezak@gmail.com>
2023-04-05 11:36:47 +0300

    comiit

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -10,9 +10,7 @@ import 'package:http/http.dart' as ht;
 import 'package:http/http.dart';
 import '../Widgets/my_button.dart';
 
-const users = {
-  'user@gmail.com': '12345',
-};
+
 bool visible = true;
 
 class LoginScreen extends StatefulWidget {
@@ -22,213 +20,209 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   Duration get loginTime => const Duration(milliseconds: 2250);
+  String il = "";
+  String ilce = "";
+  String ariCins = "";
+  String kovanCins = "";
+  var ilList = ["Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkâri", "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop", "Sivas", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak"];
 
   Future<String?> _authUser(LoginData loginData) async {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Material Dialog'),
-            content: Text('Hey! I am Coflutter!'),
-            actions: <Widget>[
-              TextButton(
-                  onPressed: () {
-
-                  },
-                  child: Text('Close')),
-              TextButton(
-                onPressed: () {
-                  print('HelloWorld!');
-
-                },
-                child: Text('HelloWorld!'),
-              )
-            ],
-          );
-        });
 
 
-
-
-
-
-    try{
-
+    try {
       ht.Response response = await post(
-          Uri.parse("http://10.0.2.2:7259/api/ValuesController1/login/Login"),
+          Uri.parse("https://reqres.in/api/login"),
           //Uri.parse("http://localhost:7259/api/ValuesController1/login/Login"),
 
-          body: {
-            "username":loginData.name,
-            "password":loginData.password
-          }
-      );
-      var jsonResponse = jsonDecode(response.body);
-      print(jsonResponse);
-      print(response.statusCode);
+          body: {"email": loginData.name, "password": loginData.password});
 
-      if (response.statusCode==200) {
+
+      if (response.statusCode == 200) {
         return null;
-      }  else{
+      } else {
         return "girişbaşarısız".tr;
-
-
       }
-    }catch(e){
+    } catch (e) {
       print(e.toString());
     }
-
   }
 
   Future<String?> _signupUser(SignupData data) {
-
     return Future.delayed(loginTime).then((_) {
-           //data.additionalSignupData!["tc"]
+      //data.additionalSignupData!["tc"]
       _showMaterialDialog();
       RegExp regex = RegExp(r'^(?=.*?).{8,}$');
-      if(!regex.hasMatch(data.password!)){
+      if (!regex.hasMatch(data.password!)) {
         print("deneme");
-        return "Şifre En Az 8 Karakterden Oluşmalıdır";
+        return "şifreuyarı8".tr;
       }
 
       return null;
     });
   }
 
-
-
-
   Future<String> _recoverPassword(String name) {
     debugPrint('Name: $name');
     return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
+      if (true) {
         return 'User not exists';
       }
       return "";
     });
   }
 
-
-
   String? _validataTC(String? value) {
-
     RegExp regex = RegExp(r'^(?=.*?).{11}$');
 
-    if(!regex.hasMatch(value!)){
-print("object");
-      return "11 Karakterden Oluşmalıdır";
+    if (!regex.hasMatch(value!)) {
+      print("object");
+      return "şifreuyarı11".tr;
     }
 
     return null;
-
   }
 
   String? _validataName(String? value) {
-
     RegExp regex = RegExp(r'^(?=.*?).{1,20}$');
 
-    if(!regex.hasMatch(value!)){
-
-      return "1 ile 20 Karakterden Oluşmalıdır";
+    if (!regex.hasMatch(value!)) {
+      return "şifreuyarı120".tr;
     }
 
     return null;
-
   }
-  String? _validataNumber(String? value) {
 
+  String? _validataNumber(String? value) {
     RegExp regex = RegExp(r'^(?=.*?).{10}$');
 
-    if(!regex.hasMatch(value!)){
-
-      return "10 Karakterden Oluşmalıdır";
+    if (!regex.hasMatch(value!)) {
+      return "şifreuyarı10".tr;
     }
 
     return null;
-
   }
 
-
   String? _validataAddress(String? value) {
-
     RegExp regex = RegExp(r'^(?=.*?).{1,200}$');
 
-    if(!regex.hasMatch(value!)){
-
-      return "1 ile 200 Karakterden Oluşmalıdır";
+    if (!regex.hasMatch(value!)) {
+      return "şifreuyarı1200".tr;
     }
 
     return null;
-
   }
 
   String? _validataBusinessNumber(String? value) {
-
     RegExp regex = RegExp(r'^(?=.*?).{14}$');
 // ilk iki rakamı 1-81 arası
-    if(!regex.hasMatch(value!)){
-
-      return "14 Karakterden Oluşmalıdır";
+    if (!regex.hasMatch(value!)) {
+      return "şifreuyarı14".tr;
     }
 
     return null;
-
   }
 
   void _showMaterialDialog() {
     showDialog(
-
+      barrierDismissible: false,
         context: context,
         builder: (context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-            title: Text('Material Dialog'),
-            content: Center(
-              child: Column(
-                children: [
-                FindDropdown(
-                items: ["Brasil", "Itália", "Estados Unidos", "Canadá"],
-                label: "País",
-                onChanged: (String? item) => print(item),
-                selectedItem: "Brasil",
+          return SizedBox(
+            width: Dimensions.width200,
+            height: Dimensions.height200,
+            child: AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
               ),
-                  FindDropdown(
-                    items: ["Brasil", "Itália", "Estados Unidos", "Canadá"],
-                    label: "País",
-                    onChanged: (String? item) => print(item),
-                    selectedItem: "Brasil",
-                  ),FindDropdown(
-                    items: ["Brasil", "Itália", "Estados Unidos", "Canadá"],
-                    label: "País",
-                    onChanged: (String? item) => print(item),
-                    selectedItem: "Brasil",
-                  ),FindDropdown(
-                    items: ["Brasil", "Itália", "Estados Unidos", "Canadá"],
-                    label: "País",
-                    onChanged: (String? item) => print(item),
-                    selectedItem: "Brasil",
-                  ),
-                ],
+              title: Text('additionalSignUpFormDescription'.tr),
+              content: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    FindDropdown(
+
+                      items: ilList,
+                      label: "il".tr,
+                      onChanged: (String? item) {
+                        il = item!;
+
+                      },
+                      searchBoxDecoration: InputDecoration(
+                        hintText: "ara".tr,
+                      ),
+
+                    ),
+                    FindDropdown(
+                      items: ["Brasil", "Itália", "Estados Unidos", "Canadá"],
+                      label: "ilçe".tr,
+                      onChanged: (String? item) {
+                        ilce = item!;
+
+                      },
+                      searchBoxDecoration: InputDecoration(
+                          hintText: "ara".tr
+                      ),
+                    ),
+                    FindDropdown(
+                      items: [
+                        "KARNİOL".tr,
+                        "KAFKAS".tr,
+                        "İTALYAN".tr,
+                        "SİYAH (ESMER)".tr,
+                        "KIBRIS".tr,
+                        "SURİYE".tr,
+                        "ANADOLU".tr,
+                        "MUĞLA".tr,
+                        "İRAN".tr,
+                        "YIĞILCA".tr,
+                      ],
+                      label: "arıcinsi".tr,
+                      onChanged: (String? item) {
+                        ariCins = item!;
+
+                      },
+                      searchBoxDecoration: InputDecoration(
+                          hintText: "ara".tr
+                      ),
+                    ),
+                    FindDropdown(
+                      items: ["eski".tr, "yeni".tr, "katlı".tr],
+                      label: "kovancinsi".tr,
+                      onChanged: (String? item) {
+                        kovanCins = item!;
+
+
+                      },
+                      searchBoxDecoration: InputDecoration(
+                          hintText: "ara".tr
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            actions: <Widget>[
-              TextButton(
+              actions: <Widget>[
+
+                TextButton(
                   onPressed: () {
+
+                    if(kovanCins.isEmpty || ariCins.isEmpty || il.isEmpty || ilce.isEmpty){
+
+                      Get.snackbar("Uyarı", "Lüften Gerekli Alanları Doldurunuz");
+
+                    }
+                    else{
+                      Get.back();
+                    }
+
+
                   },
-                  child: Text('Close')),
-              TextButton(
-                onPressed: () {
-                  print('HelloWorld!');
-                },
-                child: Text('HelloWorld!'),
-              )
-            ],
+                  child: Text('submit'.tr),
+                )
+              ],
+            ),
           );
         });
   }
-
 
   Future<String> _onSwitchToAdditionalFields(SignupData data) {
     return Future.delayed(loginTime).then((_) {
@@ -237,19 +231,11 @@ print("object");
     });
   }
 
-
-
-
-
   void changeLanguage() {
-
     Get.updateLocale(Get.locale == const Locale("tr", "TR")
         ? const Locale("en", "EN")
         : const Locale("tr", "TR"));
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -283,8 +269,8 @@ print("object");
             borderRadius: BorderRadius.circular(20),
             elevation: 12,
             child: Container(
-              width: Dimensions.width200/2,
-              height: Dimensions.height200/2,
+              width: Dimensions.width200 / 2,
+              height: Dimensions.height200 / 2,
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.all(Radius.circular(50)),
@@ -296,26 +282,56 @@ print("object");
             ),
           )),
       body: FlutterLogin(
-
-onSwitchToAdditionalFields: _onSwitchToAdditionalFields,
+        onSwitchToAdditionalFields: _onSwitchToAdditionalFields,
         additionalSignupFields: [
-          UserFormField(keyName: "tc",displayName: "tc".tr,userType: LoginUserType.phone,fieldValidator: _validataTC ),
-          UserFormField(keyName: "ad",displayName: "ad".tr,userType: LoginUserType.name,fieldValidator: _validataName),
-          UserFormField(keyName: "soyad",displayName: "soyad".tr,userType: LoginUserType.name,fieldValidator: _validataName),
-          UserFormField(keyName: "işletmeno",displayName: "işletmenumarası".tr,userType: LoginUserType.phone,fieldValidator: _validataBusinessNumber),
-          UserFormField(keyName: "ceptelefon",displayName: "ceptelefon".tr,userType: LoginUserType.phone,fieldValidator: _validataNumber ),
-          UserFormField(keyName: "adres",displayName: "adres".tr,userType: LoginUserType.name,fieldValidator: _validataAddress),
-
+          UserFormField(
+              keyName: "tc",
+              displayName: "tc".tr,
+              userType: LoginUserType.phone,
+              fieldValidator: _validataTC),
+          UserFormField(
+              keyName: "ad",
+              displayName: "ad".tr,
+              userType: LoginUserType.name,
+              fieldValidator: _validataName),
+          UserFormField(
+              keyName: "soyad",
+              displayName: "soyad".tr,
+              userType: LoginUserType.name,
+              fieldValidator: _validataName),
+          UserFormField(
+              keyName: "işletmeno",
+              displayName: "işletmenumarası".tr,
+              userType: LoginUserType.phone,
+              fieldValidator: _validataBusinessNumber),
+          UserFormField(
+              keyName: "ceptelefon",
+              displayName: "ceptelefon".tr,
+              userType: LoginUserType.phone,
+              fieldValidator: _validataNumber),
+          UserFormField(
+              keyName: "adres",
+              displayName: "adres".tr,
+              userType: LoginUserType.name,
+              fieldValidator: _validataAddress),
         ],
         userType: LoginUserType.email,
-        messages: LoginMessages(goBackButton: "geri".tr,additionalSignUpSubmitButton: "submit".tr,additionalSignUpFormDescription: "additionalSignUpFormDescription".tr,passwordHint: "login_şifre".tr,forgotPasswordButton: "login_şifremiunuttum".tr,loginButton: "login_girişyap".tr,signupButton: "login_kayıtol".tr,confirmPasswordHint: "şifretekrar".tr),
+        messages: LoginMessages(
+            goBackButton: "geri".tr,
+            additionalSignUpSubmitButton: "submit".tr,
+            additionalSignUpFormDescription:
+                "additionalSignUpFormDescription".tr,
+            passwordHint: "login_şifre".tr,
+            forgotPasswordButton: "login_şifremiunuttum".tr,
+            loginButton: "login_girişyap".tr,
+            signupButton: "login_kayıtol".tr,
+            confirmPasswordHint: "şifretekrar".tr),
         headerWidget: Center(
             child: Image.asset(
           'assets/logbee.png',
           scale: 5,
         )),
         theme: LoginTheme(
-
           cardInitialHeight: Dimensions.loginCardInitialHeight,
           buttonTheme: const LoginButtonTheme(backgroundColor: bioGreen),
           primaryColor: bioGrey,
